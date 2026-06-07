ee45a09330214922669ebb2e20d9cecae61c48e0
Suparn nayak <suparnnayak56@gmail.com>
2026-06-07 18:38:55 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int pick=nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            int n_pick=dp[i-1];
+            dp[i]=max(pick,n_pick);
+        }
+        
+    return dp[n-1];
+    }
+};
