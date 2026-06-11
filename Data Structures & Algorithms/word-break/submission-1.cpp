ecd52c597f26cd25c32d35c93111e1a4cc9a9e3c
Suparn nayak <suparnnayak56@gmail.com>
2026-06-12 02:39:38 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool f(string s, unordered_set<string>&st,vector<int>&dp,int a){
+        if(a==s.size())return true;
+        if(dp[a]!=-1)return dp[a];
+        for(int i=a;i<s.size();i++){
+            if(st.count(s.substr(a,i+1-a))){
+                dp[a]=true;
+                return true;
+            }
+        }
+        return dp[a]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=wordDict.size();
+        vector<int>dp(n+1,-1);   
+        unordered_set<string>st(wordDict.begin(),wordDict.end()); 
+        return f(s,st,dp,0);
+    }
+};
