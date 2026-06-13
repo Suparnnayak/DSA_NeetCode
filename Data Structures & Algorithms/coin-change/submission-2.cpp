a4103e0517c24301482880fdaf89dedbe2226fd1
Suparn nayak <suparnnayak56@gmail.com>
2026-06-13 16:22:16 +0530

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int s,vector<int>& coins, int amount,vector<vector<int>>&dp){
+        int n=coins.size();
+        if(amount==0)return 0;
+        if(amount <0 || s==n)return 1e9;
+        if(dp[s][amount]!=-1)return dp[s][amount];
+        int pick=1+f(s,coins,amount-coins[s],dp);
+        int n_pick=f(s+1,coins,amount,dp);
+        return dp[s][amount]=min(pick,n_pick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = f(0, coins, amount, dp);
+
+        return ans >= 1e9 ? -1 : ans;      
+    }
+};
