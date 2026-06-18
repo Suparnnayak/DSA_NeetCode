841adafc9598cd69d7866063c051f39afd304de8
Suparn nayak <suparnnayak56@gmail.com>
2026-06-18 19:37:44 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool is_palindrome(const string& s, int l, int r) {
+        while(l < r) {
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    void solve(int ind,string &s,vector<string>&temp,vector<vector<string>>&ans){
+        if(ind==s.size()){
+            ans.push_back(temp);  
+            return;
+        }  
+        for(int i=ind;i<s.size();i++){
+            if(is_palindrome(s,ind,i)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                solve(i+1,s,temp,ans);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+         vector<vector<string>>ans;
+         vector<string>temp;
+         solve(0,s,temp,ans);
+         return ans;    
+    }
+};
