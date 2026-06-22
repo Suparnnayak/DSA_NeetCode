5a900fbed5d568b21367aced5d9712ca7ee89976
Suparn nayak <suparnnayak56@gmail.com>
2026-06-22 19:06:25 +0530

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int f(int i,int j,string text1, string text2){
+        int m=text1.size();
+        int n=text2.size();
+        int p=0;int np=0;
+        if(i==m || j==n)return 0;
+        if(text1[i]==text2[j]){
+            return p=1+f(i+1,j+1,text1,text2);
+        }
+        return max(f(i+1,j,text1,text2),f(i,j+1,text1,text2));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        return f(0,0,text1,text2);    
+    }
+};
