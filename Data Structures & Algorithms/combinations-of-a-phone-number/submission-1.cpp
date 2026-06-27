a0d04dcf9be492c5d029ff1bf1de4f46dbf8cbb8
Suparn nayak <suparnnayak56@gmail.com>
2026-06-27 15:17:23 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string>ans;
+    vector<string>digittochar={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())return ans;
+        backtrack(0, "", digits);
+        return ans;   
+    }
+    void backtrack(int i,string curstr,string &digits){
+        if(curstr.size()==digits.size()){
+            ans.push_back(curstr);
+            return;
+        }
+        string chars=digittochar[digits[i]-'0'];
+        for(char c:chars){
+            backtrack(i+1,curstr+c,digits);
+        }
+    }
+};
