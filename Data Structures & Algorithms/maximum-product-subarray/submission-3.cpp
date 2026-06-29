9db18220159b784b3a25094c8c9307f7cbda99de
Suparn nayak <suparnnayak56@gmail.com>
2026-06-29 18:47:53 +0530

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+    int n = nums.size();
+    int max_sum =nums[0];
+    for (int i = 0; i < n ; i++) {
+        int current_sum = nums[i];
+        res=max(max_sum,current_sum);
+        for (int j = i+1; j < n; j++){
+            current_sum *= nums[j];
+        max_sum = max(current_sum, max_sum);
+        }
+    }
+    return max_sum;
+
+    }
+};
