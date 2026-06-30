17c7af269b329b40df2c1be5cdceed3bcd4a278e
Suparn nayak <suparnnayak56@gmail.com>
2026-06-30 09:27:27 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int r=*max_element(piles.begin(),piles.end());
+        int res=r;
+        while(l<r){
+            int k=(l+r)/2;
+            long long time=0;
+            for(int p:piles){
+                time += ceil(static_cast<double>(p) / k);
+            }
+            if(time>h){
+                l=k+1;
+            }
+            else{
+                r=k-1;
+                res=k;
+            }
+        }
+        return res;
+    }
+};
