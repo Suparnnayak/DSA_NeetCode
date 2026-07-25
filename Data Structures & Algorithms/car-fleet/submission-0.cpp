0a8717fc5591263cd11cc364d74f1947a328fcb5
Suparn nayak <suparnnayak56@gmail.com>
2026-07-25 19:05:02 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n=position.size();
+        vector<pair<int,int>>pair;
+        for(int i=0;i<n;i++){
+            pair.push_back({position[i],speed[i]});
+        }
+        sort(pair.rbegin(),pair.rend());
+        int fleet=1;
+        double  time=(target-pair[0].first)/pair[0].second;
+        for(int i=1;i<n;i++){
+            double  curtime=(target-pair[i].first)/pair[i].second;
+            if(curtime>time){
+                fleet++;
+                time=curtime;
+            }
+        }
+        return fleet;
+    }
+};
