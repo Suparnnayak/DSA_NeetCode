cece4f6c99d6e08f138c2448e36e44cb1de86873
Suparn nayak <suparnnayak56@gmail.com>
2026-07-30 01:48:15 +0530

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        unordered_set<int>good;
+        for (const auto &triplet:triplets){
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+                continue;
+            }
+        }
+    }
+};
