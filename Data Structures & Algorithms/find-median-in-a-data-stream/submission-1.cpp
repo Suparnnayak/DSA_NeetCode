8fea2898d2dc58e98fb2171b7c5479ed2bf92185
Suparn nayak <suparnnayak56@gmail.com>
2026-08-11 21:55:53 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,15 @@
+class MedianFinder {
+public:
+    vector<int>ds;
+    MedianFinder() {}
+    void addNum(int num) {
+        ds.push_back(num);
+    }
+    
+    double findMedian() {
+        sort(ds.begin(),ds.end());
+        int n=ds.size();
+        if(n&1)return ds[n/2];
+        return (ds[n/2]+ds[n/2-1])/2.0;
+    }
+};
